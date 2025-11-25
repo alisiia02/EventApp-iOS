2eb6b96316eb580183e55a72bcada46aed013063
alisiia02 <mish0001@hz.nl>
2025-11-25 18:45:40 +0100

    Initial Commit

---
+++ EventApp/EventAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventAppApp.swift
+//  EventApp
+//
+//  Created by stud on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
